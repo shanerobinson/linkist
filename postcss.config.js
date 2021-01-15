431dd8cc4d008f1c514d916420114331d6c6bdda
Shane Robinson <shane@barefeetstudios.com>
2021-01-15 16:10:53 -0700

    update postcss config for tailwind v2

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,9 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  // Specify the paths to all of the template files in your project
-  content: ["./src/**/*.njk", "./dist/**/*.html"],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
   plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
+    require(`tailwindcss`),
+    require(`autoprefixer`),
     ...(process.env.NODE_ENV === "production"
-      ? [purgecss, require("cssnano")]
+      ? [require(`cssnano`)]
       : []),
   ],
 };
